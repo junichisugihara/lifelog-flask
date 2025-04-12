0f1ff3e21cd8c2d46909a8d05b9b5c3c9fe6a481
junichisugihara <mail@junichisugihara.jp>
2025-04-12 20:23:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,3 +78,8 @@ def search_lifelog_by_keyword():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
